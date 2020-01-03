f3f6fa90d75ba428627ad85cb0aa8503c5c63b33
Gerardo Reyes <grevych@gmail.com>
2020-01-02 18:28:55 -0600

    Enable environment variables (#3)

---
+++ config/environment_variables.js
@@ -0,0 +1,12 @@
+module.exports = {
+  api: {
+    auth: {
+      domain: 'API_AUTH_DOMAIN',
+      clientId: 'API_AUTH_CLIENT_ID',
+      clientSecret: 'API_AUTH_CLIENT_SECRET',
+    },
+    cookie: {
+      secret: 'API_COOKIE_SECRET',
+    },
+  },
+};
