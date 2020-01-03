41a753601953aeacb11a1aa2b58080bcfe8e6c35
Gerardo Reyes <grevych@gmail.com>
2020-01-02 18:07:52 -0600

    Add configuration files by environment

--- config/production.js
+++ config/production.js
@@ -1,6 +1,27 @@
 module.exports = {
+  api: {
+    cors: {
+      origin: [
+        'https://esice.co',
+      ],
+      credentials: true,
+    },
+    port: 80,
+    auth: {
+      domain: '',
+      scope: 'openid email profile',
+      clientId: '',
+      clientSecret: '',
+      callbackURL: 'https://auth.esice.co/v1/callback',
+      redirectURL: 'https://esice.co',
+    },
+    cookie: {
+      secure: true,
+      secret: '',
+      domain: '.esice.com',
+    },
+  },
   database: {
-    driver: 'sql',
     database: 'db_production',
   },
 };
--- config/staging.js
+++ config/staging.js
@@ -1,7 +1,24 @@
 module.exports = {
+  api: {
+    cors: {
+      origin: [
+        'https://staging.esice.co',
+      ],
+      credentials: true,
+    },
+    port: 80,
+    auth: {
+      domain: '',
+      clientId: '',
+      clientSecret: '',
+      callbackURL: 'https://esice-auth-staging.heroku.com/v1/callback',
+    },
+    cookie: {
+      secure: true,
+      secret: 'LasCosasSonBizarras',
+    },
+  },
   database: {
-    driver: 'sql',
     database: 'db_staging',
   },
 };
-
